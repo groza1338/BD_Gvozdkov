88830ced4ad5dc36b03829275585b05d2bc0a263
groza_1337 <serezgvozdkov@mail.ru>
2024-10-17 05:00:27 +0300

    бэк в докере + переменные окружения

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5434/gvozdkov_dns_v2"
+db_port = os.getenv("DB_PORT", "5434")
+
+db_address = os.getenv("DB_ADDRESS", "localhost")
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@" + db_address + ":"+db_port+"/gvozdkov_dns_v2"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
