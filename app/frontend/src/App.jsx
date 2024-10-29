0257a7420f2dd677deb83e32f6f71d0d98f21aa5
groza_1337 <groza_1337@mail.ru>
2024-10-29 21:34:01 +0300

    теперь есть смена фавикона

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,25 +1,59 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import CategoriesPage from './components/CategoriesPage';
 import TablePage from './components/TablePage';
 import AdminPage from './components/AdminPage';
-import AnalyticsPage from './components/AnalyticsPage'; // Импорт страницы аналитики
+import AnalyticsPage from './components/AnalyticsPage';
 
 function App() {
+  useEffect(() => {
+    // Функция для изменения фавикона
+    const changeFavicon = (src) => {
+      const link = document.querySelector("link[rel~='icon']");
+      if (!link) {
+        const newLink = document.createElement('link');
+        newLink.rel = 'icon';
+        newLink.href = src;
+        document.head.appendChild(newLink);
+      } else {
+        link.href = src;
+      }
+    };
+
+    // Обработчик события visibilitychange
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        changeFavicon('/public/inactive.svg'); // Используем favicon для неактивной вкладки
+      } else {
+        changeFavicon('/public/postgres.svg'); // Используем favicon для активной вкладки
+      }
+    };
+
+    // Добавляем обработчик события visibilitychange
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    // Удаляем обработчик при размонтировании компонента
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, []);
+
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminPage />}>
-          <Route path=":tableName" element={<TablePage />} />
-        </Route>
-
-        <Route path="/analytics" element={<AnalyticsPage />} /> {/* Новый маршрут для аналитики */}
-        <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/" element={<Navigate to="/login" />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<AdminPage />}>
+            <Route path=":tableName" element={<TablePage />} />
+          </Route>
+          <Route path="/analytics" element={<AnalyticsPage />} /> {/* Новый маршрут для аналитики */}
+          <Route path="/categories" element={<CategoriesPage />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
